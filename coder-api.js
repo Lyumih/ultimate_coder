126498611246269b8dd617e0e276c5c06c0ed10a
m.ivanov@eos.local <m.ivanov@eos.local>
2019-06-25 07:32:09 +0300

    Update: add factorial func and constants

--- coder-api.js
+++ coder-api.js
@@ -7,15 +7,10 @@ function crat(item, power) {
     return item % power === 0
 }
 
-
-
-
-
-/** HASHING */
-function md5Encode(text) {
-    console.log('input', text)
-    let md5 = text
-
-    console.log('out md5 hashing', md5)
-    return md5
+function factorial(n) {
+    var result = 1;
+    while (n) {
+        result *= n--;
+    }
+    return result;
 }
